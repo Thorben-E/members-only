fdb20521d749ba7d5740be25cd64a48564a668d9
Thorben <thorbenegberink@gmail.com>
2023-01-30 17:12:30 +0000

    changed favicon path

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.deserializeUser((id, done) => {
 });
 
 const app = express();
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname + '/public/favicon.ico')))
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
